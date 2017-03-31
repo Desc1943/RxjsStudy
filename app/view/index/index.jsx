9fd0aa683201e4b315e7d4d3228bc12635468775
sayll <sayll>
2017-03-31 19:35:35 +0800

    add docs

--- app/view/index/index.jsx
+++ app/view/index/index.jsx
@@ -1,19 +1,31 @@
 import { Observable, Subject } from 'rxjs';
 
-Observable.interval(500)
-  .bufferWhen(() => Observable.interval(1000))
-  .do({
-    next: value => {
-      console.log('next1:', value);
-    },
-    complete: () => {
-      console.log(`complete1!!!`);
-    },
-    error: error => {
-      console.log(`error1:`, error);
-    }
-  })
-  .subscribe();
-// -0-1-2-3-4-5-6-7-8
-// - -c- -c- -c- -c
-// - - -c- - -c- - -c
+const source = Observable.zip(Observable.timer(1000,500),Observable.of(1,2,31,3,1), (x,y) => y);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+source.subscribe({
+  next: value => {
+    console.log('next:', value);
+  },
+  complete: () => {
+    console.log(`complete!!!`);
+  },
+  error: error => {
+    console.log(`error:`, error);
+  }
+});
